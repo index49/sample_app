c35cac4069316716f307168b7d255fbe28e4aaeb
Vi Nguyen <vi.h.nguyen@gmail.com>
2013-03-10 18:49:02 -0400

    finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -53,7 +53,6 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
-
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -69,11 +68,6 @@ describe "Authentication" do
           end
         end
       end
-
-
-
-
-
 
       describe "in the Users controller" do
 
@@ -108,6 +102,18 @@ describe "Authentication" do
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
         specify { response.should redirect_to(root_path) }
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }        
       end
     end
 
